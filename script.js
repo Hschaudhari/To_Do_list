8fbd0fa04f5faf817720ecf0176ad907529a19b2
Harshada <harshadachaudhari524@gmail.com>
2023-09-18 14:19:18 +0530

    some files are modified

--- script.js
+++ script.js
@@ -8,15 +8,31 @@ addBtn.addEventListener("click",(e)=>{
     const inputValue = input.value;
     input.value = "";
     const liItem = document.createElement("li");
+//     const dateLi = document.createElement("li");
+//     const date = new Date();
+
+//     let day = date.getDate();
+//     let month = date.getMonth() + 1;
+//     let year = date.getFullYear();
+
+//     // This arrangement can be altered based on how we want the date's format to appear.
+//     let currentDate = `${day}-${month}-${year}`;
+//     // console.log(currentDate); 
+//     const dateLiInnerHtml =  `<span class="date">Date : ${currentDate} Your todays task are</span>`
+
+//     dateLi.innerHTML = dateLiInnerHtml;
+//    ul.append(dateLi);
+
     const liInnerHtml = `<span class="text">${inputValue}</span>
-    <div>
-        <button class="btn done li-btn1">Done</button>
-        <button class="btn remove li-btn2">Remove</button>
-    </div>`
+        <div>
+            <button class="btn done li-btn1">Done</button>
+            <button class="btn remove li-btn2">Remove</button>
+        </div>`
     liItem.innerHTML = liInnerHtml;
     ul.append(liItem);
 })
 ul.addEventListener("click",(e)=>{
+    
     if(e.target.classList.contains("li-btn1")){
         const liSpan = e.target.parentNode.previousElementSibling;
         liSpan.style.textDecoration = "line-through";
@@ -25,5 +41,7 @@ ul.addEventListener("click",(e)=>{
 
     if(e.target.classList.contains("li-btn2")){
         e.target.parentNode.parentNode.remove();
+       
     }
+   
 })
